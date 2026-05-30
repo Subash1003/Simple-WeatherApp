40f3bf11b3c67d6a7863c5bf452af626376cd9f4
Subash1003 <vsubash2003.10@gmail.com>
2026-05-30 16:51:34 +0530

    updated animated images and some code in js

--- weather.js
+++ weather.js
@@ -1,16 +1,69 @@
 const apiKey = "1273ff9ff3241685d06f129d50e71db1";
 
-
 const input=document.getElementById("input");
+const clear=document.getElementById("clear");
 const searchBtn=document.getElementById("search-btn");
-
+const weatherIcon = document.getElementById("weather-icon");
 const temperature=document.getElementById("temp");
 const city=document.getElementById("cityname");
-const country= document.getElementById("location");
+const country=document.getElementById("country");
+const weathercondition= document.getElementById("weathercondition");
 const humidity=document.getElementById("humidity");
 const wind= document.getElementById("wind");
 
 city.innerText="Weather";
+
+function updateWeatherImage(condition){
+
+    switch(condition){
+
+        case "01d":
+            weatherIcon.src = "./images/clear.svg";
+            break;
+
+        case "04d":
+            weatherIcon.src = "./images/haze.svg";
+            break;
+
+        case "02d":
+        case "03d":
+            weatherIcon.src = "./images/mist.svg";
+            break;
+
+        case "04d":
+        case "04n":
+            weatherIcon.src = "./images/overcast.svg";
+            break;
+
+        case "10d":
+        case "09d":
+            weatherIcon.src = "./images/rain.svg";
+            break;
+
+        case "11d":
+        case "11n":
+            weatherIcon.src = "./images/thunder.svg";
+            break;
+            
+            case "13d":
+            case "13n":
+             weatherIcon.src = "./images/snow.svg";
+            break;
+                
+            case "50d":
+            case "50n":
+            weatherIcon.src = "./images/smoke.svg";
+            break;
+
+        case "50d":
+        case "50n":
+            weatherIcon.src = "./images/haze.svg";
+            break;
+
+        default:
+            weatherIcon.src = "./images/clear.svg";
+    }
+}
 
 async function checkWeather(queryCity) {
 
@@ -24,8 +77,10 @@ async function checkWeather(queryCity) {
         console.log(data);
 
         temperature.innerHTML = Math.round(data.main.temp) + "°C";
-        city.innerHTML = data.name , data.country;
-        country.innerHTML = "Country, "+data.sys.country;
+        city.innerHTML = data.name+", ";
+        country.innerHTML= data.sys.country;
+        weathercondition.innerHTML = data.weather[0].description;
+        updateWeatherImage(data.weather[0].icon);
         humidity.innerHTML = data.main.humidity + " %";
         wind.innerHTML = Math.round(data.wind.speed * 3.6) + " km/hr";
      } catch (err) {
@@ -42,6 +97,7 @@ searchBtn.addEventListener("click", () => {
     checkWeather(input.value);
 });
 
+
 input.addEventListener("keypress", (event) => {
 
     if(event.key === "Enter"){
@@ -49,3 +105,9 @@ input.addEventListener("keypress", (event) => {
     }
 
 });
+
+clear.addEventListener("click",() =>{
+    input.value="";
+})
+
+checkWeather("delhi");
